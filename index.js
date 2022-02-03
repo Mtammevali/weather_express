fcd6b51ce26ae5622ed8b04db1d60dcf9d2cec00
Marek Tammevali <marek.tammevali@khk.ee>
2022-02-03 10:51:37 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require ('express');
 const app = express();
 
 const path = require('path');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -32,6 +34,23 @@ app.get('/', function (req, res) {
 
 
 });
+app.post('/', function (req, res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp,
+            })
+        })
+})
 
 app.listen(3000, ()=> {
     console.log('http://localhost:3000')
